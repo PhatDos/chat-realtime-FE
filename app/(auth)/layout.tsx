1f4e6fdeef0e6f94ee3b7bcaa31c017b12d7d140
PhatDos <162654433+PhatDos@users.noreply.github.com>
2026-05-10 17:39:30 +0700

    style: sign in/sign up page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({children}: { children: React.ReactNode}) => {
     return (
-        <div className="h-full flex flex-col justify-center items-center">
+        <div className="min-h-screen w-full">
             {children}
         </div>);
 }
